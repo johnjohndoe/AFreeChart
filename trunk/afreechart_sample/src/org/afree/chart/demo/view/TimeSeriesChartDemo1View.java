cff3f3b6fe7f7df275e95ef252bcc3a523dda1c3
icomsystech.oss@gmail.com <icomsystech.oss@gmail.com@4e53b8cf-bcb5-c133-2d6e-894237e9681a>
2011-05-03 04:00:34 +0000

    separate sample source code from library project

---
+++ trunk/afreechart_sample/src/org/afree/chart/demo/view/TimeSeriesChartDemo1View.java
@@ -0,0 +1,178 @@
+/* ===========================================================
+ * AFreeChart : a free chart library for Android(tm) platform.
+ *              (based on AFreeChart and JCommon)
+ * ===========================================================
+ *
+ * (C) Copyright 2010, by Icom Systech Co., Ltd.
+ * (C) Copyright 2000-2008, by Object Refinery Limited and Contributors.
+ *
+ * Project Info:
+ *    AFreeChart: http://code.google.com/p/afreechart/
+ *    JFreeChart: http://www.jfree.org/jfreechart/index.html
+ *    JCommon   : http://www.jfree.org/jcommon/index.html
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * [Android is a trademark of Google Inc.]
+ *
+ * -----------------
+ * TimeSeriesChartDemo1View.java
+ * -----------------
+ * (C) Copyright 2010, by Icom Systech Co., Ltd.
+ *
+ * Original Author:  Niwano Masayoshi (for Icom Systech Co., Ltd);
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * 19-Nov-2010 : Version 0.0.1 (NM);
+ */ 
+
+package org.afree.chart.demo.view;
+
+import java.text.SimpleDateFormat;
+
+import org.afree.chart.ChartFactory;
+import org.afree.chart.AFreeChart;
+import org.afree.chart.axis.DateAxis;
+import org.afree.chart.demo.DemoView;
+import org.afree.chart.plot.XYPlot;
+import org.afree.chart.renderer.xy.XYItemRenderer;
+import org.afree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.afree.data.time.Month;
+import org.afree.data.time.TimeSeries;
+import org.afree.data.time.TimeSeriesCollection;
+import org.afree.data.xy.XYDataset;
+import org.afree.graphics.SolidColor;
+import org.afree.ui.RectangleInsets;
+
+import android.content.Context;
+import android.graphics.Color;
+
+/**
+ * PieChartDemo1View
+ */
+public class TimeSeriesChartDemo1View extends DemoView {
+
+    /**
+     * constructor
+     * @param context
+     */
+    public TimeSeriesChartDemo1View(Context context) {
+        super(context);
+
+        final AFreeChart chart = createChart(createDataset());
+
+        setChart(chart);
+    }
+
+    /**
+     * Creates a chart.
+     *
+     * @param dataset  a dataset.
+     *
+     * @return A chart.
+     */
+    private static AFreeChart createChart(XYDataset dataset) {
+
+        AFreeChart chart = ChartFactory.createTimeSeriesChart(
+            "Legal & General Unit Trust Prices",  // title
+            "Date",             // x-axis label
+            "Price Per Unit",   // y-axis label
+            dataset,            // data
+            true,               // create legend?
+            true,               // generate tooltips?
+            false               // generate URLs?
+        );
+
+        chart.setBackgroundPaintType(new SolidColor(Color.WHITE));
+
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaintType(new SolidColor(Color.LTGRAY));
+        plot.setDomainGridlinePaintType(new SolidColor(Color.WHITE));
+        plot.setRangeGridlinePaintType(new SolidColor(Color.WHITE));
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+
+        XYItemRenderer r = plot.getRenderer();
+        if (r instanceof XYLineAndShapeRenderer) {
+            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
+            renderer.setBaseShapesVisible(true);
+            renderer.setBaseShapesFilled(true);
+            renderer.setDrawSeriesLineAsPath(true);
+        }
+
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("MMM-yyyy"));
+
+        return chart;
+
+    }
+
+    /**
+     * Creates a dataset, consisting of two series of monthly data.
+     *
+     * @return The dataset.
+     */
+    private static XYDataset createDataset() {
+
+        TimeSeries s1 = new TimeSeries("L&G European Index Trust");
+        s1.add(new Month(2, 2001), 181.8);
+        s1.add(new Month(3, 2001), 167.3);
+        s1.add(new Month(4, 2001), 153.8);
+        s1.add(new Month(5, 2001), 167.6);
+        s1.add(new Month(6, 2001), 158.8);
+        s1.add(new Month(7, 2001), 148.3);
+        s1.add(new Month(8, 2001), 153.9);
+        s1.add(new Month(9, 2001), 142.7);
+        s1.add(new Month(10, 2001), 123.2);
+        s1.add(new Month(11, 2001), 131.8);
+        s1.add(new Month(12, 2001), 139.6);
+        s1.add(new Month(1, 2002), 142.9);
+        s1.add(new Month(2, 2002), 138.7);
+        s1.add(new Month(3, 2002), 137.3);
+        s1.add(new Month(4, 2002), 143.9);
+        s1.add(new Month(5, 2002), 139.8);
+        s1.add(new Month(6, 2002), 137.0);
+        s1.add(new Month(7, 2002), 132.8);
+
+        TimeSeries s2 = new TimeSeries("L&G UK Index Trust");
+        s2.add(new Month(2, 2001), 129.6);
+        s2.add(new Month(3, 2001), 123.2);
+        s2.add(new Month(4, 2001), 117.2);
+        s2.add(new Month(5, 2001), 124.1);
+        s2.add(new Month(6, 2001), 122.6);
+        s2.add(new Month(7, 2001), 119.2);
+        s2.add(new Month(8, 2001), 116.5);
+        s2.add(new Month(9, 2001), 112.7);
+        s2.add(new Month(10, 2001), 101.5);
+        s2.add(new Month(11, 2001), 106.1);
+        s2.add(new Month(12, 2001), 110.3);
+        s2.add(new Month(1, 2002), 111.7);
+        s2.add(new Month(2, 2002), 111.0);
+        s2.add(new Month(3, 2002), 109.6);
+        s2.add(new Month(4, 2002), 113.2);
+        s2.add(new Month(5, 2002), 111.6);
+        s2.add(new Month(6, 2002), 108.8);
+        s2.add(new Month(7, 2002), 101.6);
+
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(s1);
+        dataset.addSeries(s2);
+
+        return dataset;
+
+    }
+}
